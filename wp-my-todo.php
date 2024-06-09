b8a0c664f24b1b56d081a17d2d0e52e651251b6e
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-06-09 16:04:34 +0600

    do_action, add_action

--- wp-my-todo.php
+++ wp-my-todo.php
@@ -21,13 +21,13 @@ if( !defined( constant_name: 'ABSPATH')){
 
  
  register_activation_hook(
-    var_dump(__FILE__) ,
+    __FILE__ ,
      'my_todo_active_pluginprefix'
     );
 
 function my_todo_active_pluginprefix(){
-    // var_dump('activate');
-    // exit();
+    var_dump('activate');
+    // add_action('wp_todo_plugin_activated', 'hello');
 }
 register_deactivation_hook(
     __FILE__,
@@ -36,8 +36,43 @@ register_deactivation_hook(
 
 function my_todo_deactivation_pluginprefix()
 {
-    // var_dump('deactivation');
+    var_dump('deactivation');
+    exit();
+}
+
+register_uninstall_hook(
+    __FILE__,
+    'wp_todo_plugin_register_uninstall'
+);
+
+function wp_todo_plugin_register_uninstall(){
+    //var_dump('uninstall');
+    //exit();
+}
+
+//=========================Action Hoke===========================
+add_action('save_post', 'wp_todo_plugin_our_custom_action_to_update_post');
+
+function wp_todo_plugin_our_custom_action_to_update_post(){
+    // return('save_post');
     // exit();
 }
 
+add_action('wp_todo_plugin_activated', 'wp_todo_plugin_create_table');
+
+function wp_todo_plugin_create_table($data){
+    var_dump('table created');
+    var_dump($data);
+    exit();
+}
+
+
+
+
+
+
+
 ?>
+
+
+
